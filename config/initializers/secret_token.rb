858a5609346cf9da078070131611763a5945e4d3
Michael <michael@kitty.(none)>
2012-08-09 04:53:03 +0300

    changed greeting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gurucoder::Application.config.secret_token = 'cff2f0748488010f6f5df7ba17f967c13c087935c75b4234370a6d051402f0fb96e738dae31edce7720ee23daa41e6e5e60bf3e0e4eb50db2f3e94a589ec3740'
